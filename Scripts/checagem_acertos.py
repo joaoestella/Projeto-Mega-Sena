f108bc505393d7af71fedbfac60923c5b213d1f9
João Vítor Estella de França <jvfranca2004@gmail.com>
2025-04-22 11:10:26 -0300

    melhoria: adicionar mensagem quando não houver acertos

--- Scripts/checagem_acertos.py
+++ Scripts/checagem_acertos.py
@@ -1,4 +1,3 @@
-# Importa a função responsável por gerar uma aposta aleatória de 6 dezenas
 import json
 
 # Função que verifica se os números gerados já saíram juntos em algum concurso da Mega-Sena
@@ -13,6 +12,8 @@ def verificar_se_numeros_ja_sairam(numeros_gerados):
         numeros_gerados_str = {str(num).zfill(2) for num in numeros_gerados}
         dezenas_gerada = set(numeros_gerados_str)
 
+        encontrou_acerto = False  # Flag para verificar se houve algum acerto
+
         # Percorre todos os concursos e verifica se houve interseção significativa (quadra, quina ou sena)
         for concurso in concursos:
             dezenas_concurso = set(concurso["dezenas"])
@@ -21,17 +22,22 @@ def verificar_se_numeros_ja_sairam(numeros_gerados):
 
             match tamanho:
                 case 4:
+                    encontrou_acerto = True
                     print(f'🎯 Acertou a QUADRA!\nNúmeros Gerados: {sorted(numeros_gerados)}\n'
                           f'Acertos: {sorted(intersecao)}\nConcurso: {concurso["concurso"]} - {concurso["data"]}\n')
                 case 5:
+                    encontrou_acerto = True
                     print(f'🔥 Acertou a QUINA!\nNúmeros Gerados: {sorted(numeros_gerados)}\n'
                           f'Acertos: {sorted(intersecao)}\nConcurso: {concurso["concurso"]} - {concurso["data"]}\n')
                 case 6:
+                    encontrou_acerto = True
                     print(f'🏆 ACERTOU A SENA!!!\nNúmeros Gerados: {sorted(numeros_gerados)}\n'
                           f'Acertos: {sorted(intersecao)}\nConcurso: {concurso["concurso"]} - {concurso["data"]}\n')
+
+        if not encontrou_acerto:
+            print(f'❌ Nenhum jogo com quadra, quina ou sena encontrado.\nNúmeros: {sorted(numeros_gerados)}\n')
 
     except FileNotFoundError:
         print("❌ Erro: Arquivo mega-sena-todos-concursos.json não encontrado.")
     except json.JSONDecodeError:
         print("❌ Erro: Problema ao ler o arquivo JSON.")
-
